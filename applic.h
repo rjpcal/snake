bb474093c59fbf48ebd8b17df5c6e16e1628d835
Robert J. Peters <rjpcal@gmail.com>
2003-05-06 23:27:43 +0000

    Unneeded files.

--- applic.h
+++
@@ -1,8 +0,0 @@
-
-extern int   DISPLAY_SET_NUMBER;
-extern int   DISPLAY_COUNT;
-
-extern void InitApplication( void );
-extern void WrapApplication( void );
-extern void WriteApplication( void );
-
