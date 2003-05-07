60d4213a77ca90da9cd9a9d44b94ced4a1bf2051
Robert J. Peters <rjpcal@gmail.com>
2003-05-07 20:00:41 +0000

    Whitespace.

--- geom.c
+++ geom.c
@@ -4,34 +4,34 @@
 
 float Zerototwopi( float angle )
 {
-    while( angle < 0. )
-        angle += (2*M_PI);
+  while( angle < 0. )
+    angle += (2*M_PI);
 
-    while( angle >= (2*M_PI) )
-        angle -= (2*M_PI);
+  while( angle >= (2*M_PI) )
+    angle -= (2*M_PI);
 
-    return( angle );
+  return( angle );
 }
 
 float Minuspitopi( float angle )
 {
-    while( angle < -M_PI )
-        angle += (2*M_PI);
+  while( angle < -M_PI )
+    angle += (2*M_PI);
 
-    while( angle >= M_PI )
-        angle -= (2*M_PI);
+  while( angle >= M_PI )
+    angle -= (2*M_PI);
 
-    return( angle );
+  return( angle );
 }
 
 float Zerotopi( float angle )
 {
-    while( angle < 0. )
-        angle += M_PI;
+  while( angle < 0. )
+    angle += M_PI;
 
-    while( angle >= M_PI )
-        angle -= M_PI;
+  while( angle >= M_PI )
+    angle -= M_PI;
 
-    return( angle );
+  return( angle );
 }
 
