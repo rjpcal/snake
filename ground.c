2fddc634903eca7330bebbf1d6194c4db202e3bf
Robert J. Peters <rjpcal@gmail.com>
2003-05-07 20:07:59 +0000

    Whitespace cleanup.

--- ground.c
+++ ground.c
@@ -14,28 +14,28 @@ namespace
   const double SQRT3 = 1.7320508075;
 }
 
-int Ground::tooClose( int upto, float x, float y, int except )
+int Ground::tooClose(int upto, float x, float y, int except)
 {
-  for(int n = 0; n < upto; ++n)
+  for (int n = 0; n < upto; ++n)
     {
-      float dx = fabs( array[n].xpos - x );
-      if( dx > halfX )
+      float dx = fabs(array[n].xpos - x);
+      if (dx > halfX)
         dx = sizeX - dx;
 
-      if( dx > backgMinSpacing )
+      if (dx > backgMinSpacing)
         continue;
 
-      float dy = fabs( array[n].ypos - y );
-      if( dy > halfY )
+      float dy = fabs(array[n].ypos - y);
+      if (dy > halfY)
         dy = sizeY - dy;
 
-      if( dy > backgMinSpacing )
+      if (dy > backgMinSpacing)
         continue;
 
-      if( dx*dx+dy*dy > backgMinSpacingSqr )
+      if (dx*dx+dy*dy > backgMinSpacingSqr)
         continue;
 
-      if( n == except )
+      if (n == except)
         continue;
 
       return 1;
@@ -48,11 +48,11 @@ void Ground::insideElements()
 {
   patchNumber = snake.getLength();
 
-  for(int n = snake.getLength(); n < NPatch; ++n)
+  for (int n = snake.getLength(); n < NPatch; ++n)
     {
       int side = 1;
 
-      for(int i = 0; i < snake.getLength(); ++i)
+      for (int i = 0; i < snake.getLength(); ++i)
         {
           const int j = (i+1) % snake.getLength();
 
@@ -64,11 +64,11 @@ void Ground::insideElements()
 
           const float vp = Xij*Xin + Yij*Yin;
 
-          if( vp < 0.0 )
+          if (vp < 0.0)
             side = 0;
         }
 
-      if( side )
+      if (side)
         {
           array[n].flag = 1;
           ++patchNumber;
@@ -78,11 +78,11 @@ void Ground::insideElements()
 
 void Ground::contourElements()
 {
-  for(int n = 0; n < snake.getLength(); ++n)
+  for (int n = 0; n < snake.getLength(); ++n)
     {
       float x, y, theta;
 
-      if( snake.getElement( n, &x, &y, &theta ) )
+      if (snake.getElement(n, &x, &y, &theta))
         {
           array[NPatch].flag  = 2;
           array[NPatch].xpos  = x;
@@ -92,9 +92,9 @@ void Ground::contourElements()
         }
     }
 
-  if( NPatch > MAX_GABOR_NUMBER )
+  if (NPatch > MAX_GABOR_NUMBER)
     {
-      printf( " More than %d elements!\n", MAX_GABOR_NUMBER );
+      printf(" More than %d elements!\n", MAX_GABOR_NUMBER);
       exit(1);
     }
 }
@@ -104,8 +104,8 @@ void Ground::gridElements()
   const float dx = backgIniSpacing;
   const float dy = SQRT3 * backgIniSpacing / 2.0;
 
-  const int nx = (int)( ( 2.0 * halfX - backgMinSpacing - 0.5*dx ) / dx );
-  const int ny = (int)( ( 2.0 * halfY - backgMinSpacing ) / dy );
+  const int nx = (int)((2.0 * halfX - backgMinSpacing - 0.5*dx) / dx);
+  const int ny = (int)((2.0 * halfY - backgMinSpacing) / dy);
 
   const float ix =  -0.5 * (nx-1) * dx - 0.25 * dx;
   const float iy =  -0.5 * (ny-1) * dy;
@@ -113,11 +113,11 @@ void Ground::gridElements()
   int i, j;
   float x, y;
 
-  for(j = 0, y = iy; j < ny; ++j, y += dy)
+  for (j = 0, y = iy; j < ny; ++j, y += dy)
     {
-      for(i = 0, x = ix+0.5*(j%2)*dx; i < nx; ++i, x += dx)
+      for (i = 0, x = ix+0.5*(j%2)*dx; i < nx; ++i, x += dx)
         {
-          if( tooClose( snake.getLength(), x, y, snake.getLength()+1 ) )
+          if (tooClose(snake.getLength(), x, y, snake.getLength()+1))
             continue;
 
           array[NPatch].flag = 0;
@@ -129,9 +129,9 @@ void Ground::gridElements()
         }
     }
 
-  if( NPatch > MAX_GABOR_NUMBER )
+  if (NPatch > MAX_GABOR_NUMBER)
     {
-      printf( " More than %d elements!\n", MAX_GABOR_NUMBER );
+      printf(" More than %d elements!\n", MAX_GABOR_NUMBER);
       exit(1);
     }
 }
@@ -141,12 +141,12 @@ void Ground::fillElements()
   const int    tryFillNumber = 250000;
   const double tryFillArea   = 6.0;
 
-  int ntry = (int)( sizeX * sizeY / tryFillArea );
+  int ntry = (int)(sizeX * sizeY / tryFillArea);
 
-  if( ntry > tryFillNumber )
+  if (ntry > tryFillNumber)
     {
-      printf( " tryFillNumber too small!\n" );
-      printf( "\n Need %d, have %d\n", ntry, tryFillNumber );
+      printf(" tryFillNumber too small!\n");
+      printf("\n Need %d, have %d\n", ntry, tryFillNumber);
       exit(1);
     }
 
@@ -155,10 +155,10 @@ void Ground::fillElements()
   float xl[ tryFillNumber ];
   float yl[ tryFillNumber ];
 
-  const float dx = (float) sqrt((double) tryFillArea );
+  const float dx = (float) sqrt((double) tryFillArea);
 
-  for(float x = -halfX; x <= halfX; x += dx)
-    for(float y = -halfY; y <= halfY; y += dx)
+  for (float x = -halfX; x <= halfX; x += dx)
+    for (float y = -halfY; y <= halfY; y += dx)
       {
         xl[ ntry ] = x;
         yl[ ntry ] = y;
@@ -167,9 +167,9 @@ void Ground::fillElements()
 
   int npts = NPatch;
 
-  for(int i = 0; i < ntry; ++i)
+  for (int i = 0; i < ntry; ++i)
     {
-      if( tooClose( npts, xl[i], yl[i], npts+1 ) )
+      if (tooClose(npts, xl[i], yl[i], npts+1))
         continue;
 
       array[npts].flag  = 0;
@@ -179,14 +179,14 @@ void Ground::fillElements()
       npts++;
     }
 
-  if( npts > MAX_GABOR_NUMBER )
+  if (npts > MAX_GABOR_NUMBER)
     {
-      printf( " More than %d elements: %d\n", MAX_GABOR_NUMBER, npts );
+      printf(" More than %d elements: %d\n", MAX_GABOR_NUMBER, npts);
       exit(1);
     }
 
   backgAveSpacing = sqrt((double)2.0*sizeX*sizeY/(SQRT3*npts));
-  printf(" added %d to ave spacing %f\n", npts-NPatch, backgAveSpacing );
+  printf(" added %d to ave spacing %f\n", npts-NPatch, backgAveSpacing);
 
   NPatch = npts;
 }
@@ -197,9 +197,9 @@ void Ground::jitterElement()
 
   const int backgroundIters = 1000;
 
-  for(int niter = 0; niter < backgroundIters; ++niter)
+  for (int niter = 0; niter < backgroundIters; ++niter)
     {
-      for(int n = snake.getLength(); n < NPatch; ++n)
+      for (int n = snake.getLength(); n < NPatch; ++n)
         {
           const float dx = 2.*jitter*drand48() - jitter;
           const float dy = 2.*jitter*drand48() - jitter;
@@ -207,12 +207,12 @@ void Ground::jitterElement()
           float x  = array[n].xpos + dx;
           float y  = array[n].ypos + dy;
 
-          if( !tooClose(NPatch, x, y, n) )
+          if (!tooClose(NPatch, x, y, n))
             {
-              if( x < -halfX ) x += 2.*halfX;
-              if( x >  halfX ) x -= 2.*halfX;
-              if( y < -halfY ) y += 2.*halfY;
-              if( y >  halfY ) y -= 2.*halfY;
+              if (x < -halfX) x += 2.*halfX;
+              if (x >  halfX) x -= 2.*halfX;
+              if (y < -halfY) y += 2.*halfY;
+              if (y >  halfY) y -= 2.*halfY;
 
               array[n].xpos = x;
               array[n].ypos = y;
@@ -244,7 +244,7 @@ Ground::Ground(const Snake& s, int sizeX_, int sizeY_,
 
   const int diffusionCycles = 10;
 
-  for(int i = 0; i < diffusionCycles; ++i)
+  for (int i = 0; i < diffusionCycles; ++i)
     {
       jitterElement();
       fillElements();
@@ -252,8 +252,8 @@ Ground::Ground(const Snake& s, int sizeX_, int sizeY_,
 
   insideElements();
 
-  printf( " FOREG_NUMBER %d    PATCH_NUMBER %d    TOTAL_NUMBER %d\n",
-          snake.getLength(), patchNumber, NPatch );
+  printf(" FOREG_NUMBER %d    PATCH_NUMBER %d    TOTAL_NUMBER %d\n",
+          snake.getLength(), patchNumber, NPatch);
   backgNumber = NPatch - patchNumber;
 }
 
@@ -261,20 +261,20 @@ void Ground::renderInto(FakeWindow* w, const GaborSet& g) const
 {
   w->clear(0.0);
 
-  for(int i = 0; i < NPatch; ++i)
+  for (int i = 0; i < NPatch; ++i)
     {
       const float phi   = 2 * M_PI * drand48();
       const float randtheta = 2 * M_PI * drand48();
 
       const float theta =
         (i < snake.getLength())
-        ? Zerototwopi( array[i].theta + M_PI_2 )
+        ? Zerototwopi(array[i].theta + M_PI_2)
         : randtheta;
 
-      const int xcenter = (int)( array[i].xpos + sizeX / 2.0 + 0.5 );
-      const int ycenter = (int)( array[i].ypos + sizeY / 2.0 + 0.5 );
+      const int xcenter = (int)(array[i].xpos + sizeX / 2.0 + 0.5);
+      const int ycenter = (int)(array[i].ypos + sizeY / 2.0 + 0.5);
 
-      const double* p = g.getPatch( theta, phi );
+      const double* p = g.getPatch(theta, phi);
 
       // bottom left:
       const int x0  = xcenter - g.getPatchSize() / 2;
@@ -294,7 +294,7 @@ void Ground::writeArray(const char* filestem, int displayCount) const
   int x, y, o, s;
 
   char fname[STRINGSIZE];
-  sprintf( fname, "%s.snk", filestem);
+  sprintf(fname, "%s.snk", filestem);
 
   FILE* fp = fopen(fname, "a");
   if (fp == 0)
@@ -314,35 +314,35 @@ void Ground::writeArray(const char* filestem, int displayCount) const
 
   const double RAD2DEG = (180./M_PI);
 
-  for(int i = 0; i < NPatch; ++i)
+  for (int i = 0; i < NPatch; ++i)
     {
-      if( this->flag(i) )
+      if (this->flag(i))
         {
-          o = (int)( RAD2DEG * this->theta(i) + 0.5 );
+          o = (int)(RAD2DEG * this->theta(i) + 0.5);
 
-          x = (int)( this->xpos(i) + 0.5 );
+          x = (int)(this->xpos(i) + 0.5);
 
-          y = (int)( this->ypos(i) + 0.5 );
+          y = (int)(this->ypos(i) + 0.5);
 
           s = this->flag(i);
 
-          fprintf( fp, "%-5d %-5d %-5d %-5d\n", x, y, o, s );
+          fprintf(fp, "%-5d %-5d %-5d %-5d\n", x, y, o, s);
         }
     }
 
-  for(int i = 0; i < NPatch; ++i)
+  for (int i = 0; i < NPatch; ++i)
     {
-      if( !this->flag(i) )
+      if (!this->flag(i))
         {
-          o = (int)( RAD2DEG * this->theta(i) + 0.5 );
+          o = (int)(RAD2DEG * this->theta(i) + 0.5);
 
-          x = (int)( this->xpos(i) + 0.5 );
+          x = (int)(this->xpos(i) + 0.5);
 
-          y = (int)( this->ypos(i) + 0.5 );
+          y = (int)(this->ypos(i) + 0.5);
 
           s = this->flag(i);
 
-          fprintf( fp, "%-5d %-5d %-5d %-5d\n", x, y, o, s );
+          fprintf(fp, "%-5d %-5d %-5d %-5d\n", x, y, o, s);
         }
     }
 
