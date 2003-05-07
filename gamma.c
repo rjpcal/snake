4aabcd8757fe4f5ac450741cd425d0ff946b22e8
Robert J. Peters <rjpcal@gmail.com>
2003-05-07 00:41:44 +0000

    Cleanup colormap gamma stuff.

--- gamma.c
+++ gamma.c
@@ -25,23 +25,113 @@
 
 int White, Grey, Black;
 
-Colorindex colindex[LUMI_LEVELS]; /* contains colindex values for each lumi */
-
-double lumi[MAX_COLINDEX]; /*contains lumi val( 0 -> 1) for each index; */
-
-struct cmapval { unsigned char r, g, b; };
-cmapval colormap[MAX_COLINDEX];
-
-void nrerror(char error_text[]);
-
-Colorindex F2I(double fraction)
+namespace
 {
-    return colindex[(int) rint((fraction+1.0)/2.0 * (LUMI_LEVELS-1))];
-}
+  void nrerror(char error_text[])
+  {
+    fprintf(stderr,"%s\n",error_text);
+    exit(1);
+  }
 
-unsigned char I2Char( Colorindex index )
-{
-    return( (unsigned char)((int)(255.0*lumi[index]) % 256) );
+  float gammln(float xx)
+  {
+    double x,y,tmp,ser;
+    static double cof[6] = {76.18009172947146,-86.50532032941677,
+                            24.01409824083091,-1.231739572450155,
+                            0.1208650973866179e-2,-0.5395239384953e-5};
+    int j;
+
+    y=x=xx;
+    tmp=x+5.5;
+    tmp -= (x+0.5)*log(tmp);
+    ser=1.000000000190015;
+    for (j=0;j<=5;j++) ser += cof[j]/++y;
+    return -tmp+log(2.5066282746310005*ser/x);
+  }
+
+  void gcf(float *gammcf,float a, float x, float *gln)
+  {
+    int i;
+    float an, b,c,d,del,h;
+
+    *gln=gammln(a);
+    b=x=1.0-a;
+    c=1.0/FPMIN;
+    d=1.0/b;
+    h=d;
+    for (i=1; i < ITMAX; i++){
+      an = -i*(i-a);
+      b += 2.0;
+      d = an*d+b;
+      if (fabs(d) < FPMIN) d=FPMIN;
+      c=b+an/c;
+      if (fabs(c) < FPMIN) c=FPMIN;
+      d=1.0/d;
+      del=d*c;
+      h *= del;
+      if (fabs(del-1.0) < EPS) break;
+    }
+    if (i > ITMAX) nrerror("a too large, ITMAX too small in gcf");
+    *gammcf=exp(-x+a*log(x)-(*gln))*h;
+  }
+
+  void gser(float *gamser, float a, float x, float *gln)
+  {
+    int n;
+    float sum,del,ap;
+
+    *gln=gammln(a);
+    if (x <= 0.0) {
+      if (x < 0.0) nrerror("x less than 0 in routine gser");
+      *gamser=0.0;
+      return;
+    } else {
+      ap=a;
+      del=sum=1.0/a;
+      for (n=1;n<=ITMAX;n++) {
+        ++ap;
+        del *= x/ap;
+        sum += del;
+        if (fabs(del) < fabs(sum)*EPS){
+          *gamser=sum*exp(-x+a*log(x)-(*gln));
+          return;
+        }
+      }
+      nrerror("a too large, ITMAX too small in routine gser");
+      return;
+    }
+  }
+
+  float gammp(float a, float x)
+  {
+    float gamser, gammcf, gln;
+
+    if (x < 0.0 || a <= 0.0) nrerror("Invalid arguments in routine gammp");
+
+    if(x < (a+1.0)) {
+      gser(&gamser, a,x,&gln);
+      return gamser;
+    } else {
+      gcf(&gammcf,a,x,&gln);
+      return 1.0 - gammcf;
+    }
+  }
+
+  float luminance(short r, short g, short b)
+    /* returns value between 0.0 and 1.0 */
+  {
+    return gammp(A_GAMMA,(float) r /X_FAC) * Y_FAC*RRATIO +
+      gammp(A_GAMMA,(float) g /X_FAC) * Y_FAC*GRATIO +
+      gammp(A_GAMMA,(float) b /X_FAC) * Y_FAC*BRATIO;
+  }
+
+
+  Colorindex colindex[LUMI_LEVELS]; /* contains colindex values for each lumi */
+
+  double lumi[MAX_COLINDEX]; /*contains lumi val( 0 -> 1) for each index; */
+
+  struct cmapval { unsigned char r, g, b; };
+  cmapval colormap[MAX_COLINDEX];
 }
 
 void MakeColormap(void)
@@ -104,114 +194,12 @@ void MakeColormap(void)
 
 }
 
-
-float luminance(short r, short g, short b)
-     /* returns value between 0.0 and 1.0 */
+Colorindex F2I(double fraction)
 {
-
-  float lumi;
-
-  float gammp(float a, float x);
-
-  lumi = gammp(A_GAMMA,(float) r /X_FAC) * Y_FAC*RRATIO +
-    gammp(A_GAMMA,(float) g /X_FAC) * Y_FAC*GRATIO +
-    gammp(A_GAMMA,(float) b /X_FAC) * Y_FAC*BRATIO;
-
-   return lumi;
+  return colindex[(int) rint((fraction+1.0)/2.0 * (LUMI_LEVELS-1))];
 }
 
-float gammln(float xx)
+unsigned char I2Char( Colorindex index )
 {
-  double x,y,tmp,ser;
-  static double cof[6] = {76.18009172947146,-86.50532032941677,
-                          24.01409824083091,-1.231739572450155,
-                          0.1208650973866179e-2,-0.5395239384953e-5};
-  int j;
-
-  y=x=xx;
-  tmp=x+5.5;
-  tmp -= (x+0.5)*log(tmp);
-  ser=1.000000000190015;
-  for (j=0;j<=5;j++) ser += cof[j]/++y;
-  return -tmp+log(2.5066282746310005*ser/x);
-}
-
-float gammp(float a, float x)
-{
-  void gfc(float *gammcf, float a, float x, float *gln);
-  void gser(float *gamser, float a, float s, float *gln);
-  float gamser, gammcf, gln;
-
-  if (x < 0.0 || a <= 0.0) nrerror("Invalid arguments in routine gammp");
-
-  if(x < (a+1.0)) {
-    gser(&gamser, a,x,&gln);
-    return gamser;
-  } else {
-    gfc(&gammcf,a,x,&gln);
-    return 1.0 - gammcf;
-  }
-}
-
-
-void gser(float *gamser, float a, float x, float *gln)
-{
-  float gammln(float xx);
-  int n;
-  float sum,del,ap;
-
-  *gln=gammln(a);
-  if (x <= 0.0) {
-    if (x < 0.0) nrerror("x less than 0 in routine gser");
-    *gamser=0.0;
-    return;
-  } else {
-    ap=a;
-    del=sum=1.0/a;
-    for (n=1;n<=ITMAX;n++) {
-      ++ap;
-      del *= x/ap;
-      sum += del;
-      if (fabs(del) < fabs(sum)*EPS){
-        *gamser=sum*exp(-x+a*log(x)-(*gln));
-        return;
-      }
-    }
-    nrerror("a too large, ITMAX too small in routine gser");
-    return;
-  }
-}
-
-
-void gfc(float *gammcf,float a, float x, float *gln)
-{
-  float gammln(float xx);
-  int i;
-  float an, b,c,d,del,h;
-
-  *gln=gammln(a);
-  b=x=1.0-a;
-  c=1.0/FPMIN;
-  d=1.0/b;
-  h=d;
-  for (i=1; i < ITMAX; i++){
-    an = -i*(i-a);
-    b += 2.0;
-    d = an*d+b;
-    if (fabs(d) < FPMIN) d=FPMIN;
-    c=b+an/c;
-    if (fabs(c) < FPMIN) c=FPMIN;
-    d=1.0/d;
-    del=d*c;
-    h *= del;
-    if (fabs(del-1.0) < EPS) break;
-  }
-  if (i > ITMAX) nrerror("a too large, ITMAX too small in gcf");
-  *gammcf=exp(-x+a*log(x)-(*gln))*h;
-}
-
-void nrerror(char error_text[])
-{
-  fprintf(stderr,"%s\n",error_text);
-  exit(1);
+  return( (unsigned char)((int)(255.0*lumi[index]) % 256) );
 }
--- gamma.h
+++ gamma.h
@@ -2,12 +2,6 @@
 extern Colorindex F2I(double fraction);
 extern unsigned char I2Char( Colorindex index );
 extern void MakeColormap(void);
-extern float luminance(short r, short g, short b);
-extern float gammln(float xx);
-extern float gammp(float a, float x);
-extern void gser(float *gamser, float a, float x, float *gln);
-extern void gfc(float *gammcf,float a, float x, float *gln);
-extern void nrerror(char error_text[]);
 
 extern int White, Grey, Black;
 
